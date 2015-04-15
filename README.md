9cd89d7de17d036b7c6b5d458a4accdd6e72ccab
Felipe Figueiredo <felipe@code6.com.br>
2015-04-15 00:19:49 -0300

    Add arquivos repositório.

---
+++ README.md
@@ -0,0 +1,14 @@
+==== Descrição ===
+
+A integração é simples e consiste em 3 constantes que estão declaradas no 'wp-config.php', que são: RD_STATION_IDENTIFICACAO, RD_STATION_TOKEN e RD_STATION_API, que são os campos de identificador, token, e a URL da API, respectivamente.
+
+O processo de envio por post se dá por conta da função wp_remote_post, que envia uma requisição POST para a API, utilizando as funções nativas da HTTP API do WordPress para este feito.
+
+O plugin de formulário tem como padrão três inputs "Nome, Email e Telefone" juntamente com uma função shortcode para exibir o formulário nas páginas desejadas.
+
+
+=== Instalação ===
+
+Estando dentro da raiz do WordPress, baixe o plugin para dentro da pasta wp-content através do Git:
+
+git clone https://github.com/felipebuchmann/formulario-rdstation wp-content/plug-ins/plugin-form
---
+++ form-de-contato.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Plugin Name: Formulário de Contato
+Description: Um form simples.
+Version: 1.0
+Author: Felipe Figueiredo
+*/
+ 
+function html_form_code() {
+    echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
+    echo '<p>';
+    echo 'Seu nome (obrigatório) <br/>';
+    echo '<input type="text" name="input-nome" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["input-nome"] ) ? esc_attr( $_POST["input-nome"] ) : '' ) . '" size="40" />';
+    echo '</p>';
+    echo '<p>';
+    echo 'Email (obrigatório) <br/>';
+    echo '<input type="email" name="input-email" value="' . ( isset( $_POST["input-email"] ) ? esc_attr( $_POST["input-email"] ) : '' ) . '" size="40" />';
+    echo '</p>';
+    echo '<p>';
+    echo 'Telefone (obrigatório) <br/>';
+    echo '<input type="text" name="telefone" value="' . ( isset( $_POST["telefone"] ) ? esc_attr( $_POST["telefone"] ) : '' ) . '">';
+    echo '</p>';
+    echo '<p><input type="submit" name="btn-enviar" value="Enviar"></p>';
+    echo '</form>';
+}
+ 
+function deliver_mail() {
+ 
+    // if botão enviar clicado
+    if ( isset( $_POST['btn-enviar'] ) ) {
+ 
+        // sanitize valores
+        $name     = sanitize_text_field( $_POST["input-nome"] );
+        $email    = sanitize_email( $_POST["input-email"] );
+        $telefone = sanitize_text_field( $_POST["telefone"] );
+ 
+        // get the blog administrator's email address
+        $to = get_option( 'admin_email' );
+        $args = [
+          'token_rdstation' => RD_STATION_TOKEN,
+          'identificador'   => RD_STATION_IDENTIFICACAO,
+          'email'           => $email,
+          'nome'            => $name,
+          'telefone'        => $telefone,
+        ];
+ 
+        // If email enviado
+        if ( wp_remote_post(RD_STATION_API, $args)) {
+            echo '<div>';
+            echo '<p>O Formulário foi enviado com sucesso!</p>';
+            echo '</div>';
+        } else {
+            echo 'Ops, há algum erro!';
+        }
+    }
+}
+ 
+function form_shortcode() {
+    ob_start();
+    deliver_mail();
+    html_form_code();
+ 
+    return ob_get_clean();
+}
+ 
+add_shortcode( 'formulario_rd_station', 'form_shortcode' );
+ 
+?>
